889a39105756b2bc747b18c9d3755f53ed65adcc
“Ana <ana.neo@mercadolivre.com>
2020-07-31 22:27:38 -0300

    inital commit

--- src/aulaPOO.kt
+++ src/aulaPOO.kt
@@ -1,3 +1,42 @@
+class Sentimento {
+
+    var nome: String = ""
+    var intensidade: String = ""
+    var motivo: String = ""
+
+    fun ficarNervoso() {
+        this.nome = "Nervoso"
+        this.intensidade = "10"
+    }
+
+    fun ficarFeliz() {
+        this.nome = "Feliz"
+        this.intensidade = "8"
+    }
+
+
+    fun status() {
+        println("NOME: " + this.nome)
+        println("INTENSIDADE: " + this.intensidade)
+        println("MOTIVO: " + this.motivo)
+        println("-------------------------- ++ --------------------------")
+    }
+}
+
 fun main() {
+
+    val pessoaFeelings = Sentimento()
+    pessoaFeelings.nome = "Normal"
+    pessoaFeelings.intensidade = "4"
+
+    pessoaFeelings.status()
+
+    pessoaFeelings.ficarFeliz()
+
+    pessoaFeelings.status()
+
+    pessoaFeelings.ficarNervoso()
+
+    pessoaFeelings.status()
 
 }
