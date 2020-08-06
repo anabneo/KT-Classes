aee2838eff7e95200cbb63ea6241c5bd9f8b9562
“Ana <ana.neo@mercadolivre.com>
2020-08-06 17:48:23 -0300

    add cachorro object

--- AulaVisibilidade/src/Cachorro.kt
+++ AulaVisibilidade/src/Cachorro.kt
@@ -1,4 +1,18 @@
 class Cachorro {
 
+    var cor: String = ""
+    var tamanho: String = ""
+    var temperamento: String = ""
 
+    fun latir() {
+
+    }
+
+    fun correr() {
+
+    }
+
+    fun morder() {
+
+    }
 }
